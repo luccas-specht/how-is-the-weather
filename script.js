bc3e387e8b6e900d3fe824e145c56f446ac26900
luccas-specht <luccasspecht70@gmail.com>
2024-03-11 21:03:45 -0300

    feat: add render HTML elements

--- script.js
+++ script.js
@@ -46,12 +46,83 @@ const getUpcomingPredictions = async ({ city, APIKey }) => {
   return await getPredictionsBasedOnLatAndLon({ lat, lon, APIKey });
 };
 
+const icons = {};
+
+// add HTML code
 const mainCall = async () => {
+  var container = document.getElementById('weatherContainer');
+
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
   const city = document.getElementById('cityToSearch').value;
   const APIKey = 'b23965f99d8afde99c2e60db0753b5e8';
 
   const response = await getUpcomingPredictions({ city, APIKey });
-  console.log({ response });
+
+  const predictions = response.map((element) => ({
+    time: element.dt_txt,
+    weather: element.weather[0],
+    temp_max: element.main.temp_max,
+    temp_min: element.main.temp_min,
+  }));
+
+  var weatherContainer = document.getElementById('weatherContainer');
+
+  predictions.forEach((item) => {
+    var time = new Date(item.time);
+    var hours = time.getHours();
+    var formattedTime = hours < 10 ? '0' + hours : hours;
+
+    var weatherCard = document.createElement('div');
+    weatherCard.classList.add(
+      'wrapper__main-content__next-weathers__next-hours--card'
+    );
+
+    var timeElement = document.createElement('strong');
+    timeElement.textContent = formattedTime + ':00';
+
+    var weatherIconElement = document.createElement('img');
+    weatherIconElement.src = `https://openweathermap.org/img/w/${item.weather.icon}.png`;
+
+    var tempElement = document.createElement('div');
+    var maxTempElement = document.createElement('span');
+    maxTempElement.classList.add(
+      'wrapper__main-content__next-weathers__next-hours--card-max-celsius'
+    );
+    var tempMax = Math.round(item.temp_max);
+    var tempMin = Math.round(item.temp_min);
+    maxTempElement.textContent = tempMax + ' ºC';
+
+    var minTempElement = document.createElement('span');
+    minTempElement.classList.add(
+      'wrapper__main-content__next-weathers__next-hours--card-min-celsius'
+    );
+    minTempElement.textContent = tempMin + ' ºC';
+    tempElement.appendChild(maxTempElement);
+    tempElement.appendChild(minTempElement);
+
+    var weatherDescriptionElement = document.createElement('span');
+    weatherDescriptionElement.classList.add(
+      'wrapper__main-content__next-weathers__next-hours--card-prediction'
+    );
+    var description =
+      item.weather.description.charAt(0).toUpperCase() +
+      item.weather.description.slice(1);
+    weatherDescriptionElement.textContent = description;
+
+    // Adiciona os elementos ao card
+    weatherCard.appendChild(timeElement);
+    weatherCard.appendChild(weatherIconElement);
+    weatherCard.appendChild(tempElement);
+    weatherCard.appendChild(weatherDescriptionElement);
+
+    // Adiciona o card ao container
+    weatherContainer.appendChild(weatherCard);
+  });
+
+  console.log({ response, predictions });
 };
 
 document.getElementById('buttonId').addEventListener('click', mainCall);
